089347248e6d157ec09102925026e4948c7a2bb6
Carlos Vasconcelos <ceavjr@gmail.com>
2022-02-16 18:21:10 -0300

    questão 01

---
+++ DesafioCapgemini/src/Q1Escada.java
@@ -0,0 +1,27 @@
+
+/**
+ * Algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços.
+ * @author Carlos Eduardo de Albquerque Vasconcelos Junior
+ */
+import java.util.Scanner;
+
+public class Q1Escada {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int linha, coluna, numdeasteriscos;
+
+		System.out.println("Digite um número inteiro para o valor de N: ");
+		numdeasteriscos = sc.nextInt();
+
+		for (linha = 1; linha <= numdeasteriscos; linha++) {
+			for (coluna = numdeasteriscos - linha; coluna >= 1; coluna--)
+				System.out.print(" ");
+			for (coluna = 1; coluna <= linha; coluna++)
+				System.out.print("*");
+			System.out.println();
+		}
+		sc.close();
+	}
+}
